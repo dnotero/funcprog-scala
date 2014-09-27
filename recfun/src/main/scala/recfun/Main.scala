653d06ce3054d53545e2072c5e8f4a4f3d99995e
dotero <otero.diego.87@gmail.com>
2014-09-27 16:13:55 -0300

    count change

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -48,5 +48,12 @@ object Main {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def isExactChange = money == 0
+    def isNotExactChange = money < 0 || coins.isEmpty
+    
+    if(isExactChange) 1
+    else if(isNotExactChange) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
